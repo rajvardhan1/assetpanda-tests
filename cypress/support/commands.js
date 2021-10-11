63945c28f254e2fc40ed79c59d05a5e5209370b9
rajvardhan1 <rajvardhan@webkorps.com>
2021-10-12 02:21:13 +0530

    Date picker issue fixed

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,57 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+
+Cypress.Commands.add('selectYear', (yearName) => {
+
+    const currentYear = new Date().getFullYear();
+
+    cy.get('.ui-datepicker-year').then(($year) => {
+
+        cy.log($year.text(), ' year-1')
+
+        if ($year.text() == yearName) {
+            cy.log(yearName, ' year selected');
+            return;
+        }
+        else {
+
+            if (yearName < currentYear) {
+                cy.get('.ui-datepicker-prev > .ui-icon').click()
+            }
+            else if (yearName > currentYear) {
+                cy.get('.ui-datepicker-next > .ui-icon').click()
+            }
+        }
+
+        cy.selectYear(yearName)
+
+    })
+
+})
+
+Cypress.Commands.add('selectMonth', (monthName) => {
+
+    cy.get('.ui-datepicker-month').then(($month) => {
+
+        let currentMonth = new Date().getMonth + 1;
+        let givenMonth = dateUtils.getMonthIndexFromName(monthName);
+
+        if ($month.text().includes(monthName)) {
+            cy.log(monthName, ' is selected');
+            return
+        } else {
+            if (givenMonth > currentMonth) {
+                cy.get('.ui-datepicker-next > .ui-icon').click()
+            }
+            else if (givenMonth > currentMonth) {
+                cy.get('.ui-datepicker-prev > .ui-icon').click()
+            }
+        }
+
+        cy.selectMonth(monthName)
+
+    })
+
+})
